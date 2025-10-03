1b10ad70d3efdfc1000f2e9abe77cbfaa6700ea7
PravinKarthi <mpravinofficial@gmail.com>
2025-10-03 18:41:15 +0530

    Add basic test for Flask app fullyy

--- test_app.py
+++ test_app.py
@@ -2,7 +2,7 @@ import pytest
 import app
 
 def test_home():
-    client = app.test_client()
+    client = app.app.test_client()
     response = client.get("/")
     assert response.status_code == 200
     assert b"Hello, DevOps!" in response.data
