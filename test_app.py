a75a115cdb797cfd3b72e6bf9265474f2df80d4c
PravinKarthi <mpravinofficial@gmail.com>
2025-10-08 21:28:54 +0530

    blabala

--- test_app.py
+++ test_app.py
@@ -1,7 +1,6 @@
-import app
-
+from app import app 
 def test_home():
-    client = "C:/Users/91866/Documents/ci-cd-flask/app.py".test_client()
+    client = app.test_client()
     response = client.get("/")
     assert response.status_code == 200
     assert b"Hello, DevOps!" in response.data
